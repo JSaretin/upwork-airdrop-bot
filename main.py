dae4d20ebb437aea3879d281efc099abcb0dde57
v0idhack <johnosaretin7878@gmail.com>
2022-05-22 18:32:00 +0100

    bot def

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from bot import bot
+from fastapi import FastAPI
 import asyncio
+from airdrop import bot
+
+
+
+
+# bot.set_webhook('https://airdrop-bot.herokuapp.com/')
+app = FastAPI()
+
 
 if __name__ == '__main__':
     print('online')
