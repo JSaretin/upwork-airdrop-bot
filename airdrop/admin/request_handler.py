4aaee61dbebcf3e868f98bd9e61b7ca61394f364
v0idhack <johnosaretin7878@gmail.com>
2022-05-22 18:32:53 +0100

    admin request handlers

---
+++ airdrop/admin/request_handler.py
@@ -0,0 +1,107 @@
+from . import request_hook
+from airdrop import FormatedData
+@request_hook
+async def request_welcome_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_request_twitter_post_retweet_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_twitter_post_retweet_link_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_group_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_channel_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_enter_address_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_address_already_used_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_address_save_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_enter_email_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_email_already_used_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_email_save_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_enter_twitter_username_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_twitter_username_already_used_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_twitter_username_save_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_enter_verification_code_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_wrong_verification_code_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_invalid_message_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_captcha_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_dashboard_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_join_group_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_join_channel_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_ban_from_group_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_ban_from_channel_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_reply_to_address_request_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_reply_to_email_request_message(message: FormatedData, **kwargs):
+   return
+
+@request_hook
+async def request_airdrop_rules_message(message: FormatedData, **kwargs):
+   return
+
+
