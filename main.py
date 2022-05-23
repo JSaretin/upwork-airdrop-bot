3053e6f31d90e9c1eb4dc54212f5017ee495edcf
v0idhack <johnosaretin7878@gmail.com>
2022-05-23 20:02:49 +0100

    site runner

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from fastapi import FastAPI
 import asyncio
+from fastapi import FastAPI
 from airdrop import bot
+from telebot import types
 
 
-
-
-# bot.set_webhook('https://airdrop-bot.herokuapp.com/')
 app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {'message': 'Hello World'}
+
+
+@app.put('/toggle')
+async def delete():
+  await bot.remove_webhook()
+
+
+@app.post('/toggle')
+async def set_webhook():
+  await bot.set_webhook(f'https://5k28v6.deta.dev/')
+  
+
+@app.post('/')
+async def webhook(update: dict):
+    if update:
+        update = types.Update.de_json(update)
+        await bot.process_new_updates([update])
+    else:
+        return
+
 
 
 if __name__ == '__main__':
