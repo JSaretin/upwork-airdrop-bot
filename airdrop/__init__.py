7db28508e1dacf4fc8e4feac44b412a1d5d895b1
v0idhack <johnosaretin7878@gmail.com>
2022-05-24 23:15:04 +0100

    update

--- airdrop/__init__.py
+++ airdrop/__init__.py
@@ -2,8 +2,8 @@ from os import environ
 from telebot.async_telebot import AsyncTeleBot as TeleBot
 from dotenv import load_dotenv
 
-load_dotenv('.env')
-bot = TeleBot(environ['BOT_TOKEN'])
+# load_dotenv('../.env')
+bot = TeleBot('5303775819:AAGHnHmhPIUQDVxNYD9y5Z4uFZbbpwqNdwE')
 
 
 from airdrop import admin
